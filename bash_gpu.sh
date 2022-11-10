5c38bfde3c15e83643d6c734a36c016f540bacf1
Pleth <s174503@student.dtu.dk>
2022-11-10 12:43:49 +0100

    tests done

--- bash_gpu.sh
+++ bash_gpu.sh
@@ -3,7 +3,7 @@
 #BSUB -J testjob
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 10:00
+#BSUB -W 12:00
 #BSUB -R "rusage[mem=5GB]"
 #BSUB -o outputs/gpu_%J.out
 #BSUB -e outputs/errors/gpu_%J.err
@@ -16,8 +16,9 @@ module load cuda/11.6
 source $HOME/miniconda3/bin/activate
 source venv_1/bin/activate
 
-#python3 main.py Deep_google 128 1e-6 10 800 2_4_test2 7
-#python3 main.py Deep_google 128 1e-6 10 800 shuffle_test2 99
-python3 main.py CNN_simple 128 1e-6 10 60 shufflev1 99
-python3 main.py CNN_simple 128 1e-6 20 60 shufflev2 99
-python3 main.py CNN_simple 128 1e-6 30 60 shufflev3 99
+python3 main.py Deep_google 128 1e-6 15 800 2_4_test4 7
+#python3 main.py Deep_google 128 1e-6 20 800 shuffle_test5 99
+python3 main.py CNN_simple 128 1e-6 50 200 shufflev5 99
+python3 main.py CNN_simple 128 1e-6 75 200 shufflev6 99
+python3 main.py CNN_simple 128 1e-6 100 200 shufflev7 99
+
