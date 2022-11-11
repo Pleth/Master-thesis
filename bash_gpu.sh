4ce20a3cb86f0c0f78f530ca4918a26781bd5682
Pleth <s174503@student.dtu.dk>
2022-11-11 13:46:45 +0100

    test results

--- bash_gpu.sh
+++ bash_gpu.sh
@@ -18,9 +18,9 @@ source venv_1/bin/activate
 
 #python3 main.py Deep_google 128 1e-6 15 800 2_4_test4 7
 #python3 main.py Deep_google 128 1e-6 20 800 shuffle_test5 99
-python3 main.py CNN_simple 128 1e-8 0.1 100 shuffle_wd01 99 SGD
-python3 main.py CNN_simple 128 1e-8 1 100 shuffle_wd1 99 SGD
-python3 main.py CNN_simple 128 1e-8 10 100 shuffle_wd10 99 SGD
+python3 main.py CNN_simple 128 1e-8 0.1 100 shuffle1_wd01 99 SGD
+python3 main.py CNN_simple 128 1e-8 1 100 shuffle1_wd1 99 SGD
+python3 main.py CNN_simple 128 1e-8 10 100 shuffle1_wd10 99 SGD
 python3 main.py CNN_simple 128 1e-8 0.1 100 2_4_wd01 7 SGD
 python3 main.py CNN_simple 128 1e-8 1 100 2_4_wd1 7 SGD
 python3 main.py CNN_simple 128 1e-8 10 100 2_4_wd10 7 SGD
